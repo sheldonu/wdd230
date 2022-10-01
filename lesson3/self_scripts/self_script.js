549ea8080786c7a918de999b9ca13ae1ba31b9a5
sheldonu <uch20002@byui.edu>
2022-10-01 16:27:06 -0700

    w03 finished

---
+++ lesson3/self_scripts/self_script.js
@@ -0,0 +1,8 @@
+let oLastModif = new Date(document.lastModified)
+let day = oLastModif.getDate()
+let month = oLastModif.getMonth() + 1
+let year = oLastModif.getFullYear()
+let sec = oLastModif.getSeconds()
+let min = oLastModif.getMinutes()
+let hour = oLastModif.getHours()
+document.querySelector('.update').textContent = "Last Updated: " + month + "/" + day + "/" + year + " " + hour + ":" + min + ":" + sec
